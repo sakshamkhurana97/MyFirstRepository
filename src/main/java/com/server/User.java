0487cb7d08747fc83e66aa0a079f78f874469c20
train <train@TRN5479.dmztrn.amdocs.trn>
2019-08-22 11:20:08 +0530

    iii

---
+++ src/main/java/com/server/User.java
@@ -0,0 +1,9 @@
+package com.server;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+@XmlJavaTypeAdapter(UserAdapter.class)
+public interface User {
+
+    String getName();
+}
---
+++ src/main/java/com/server/UserAdapter.java
@@ -0,0 +1,19 @@
+package com.server;
+
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+
+public class UserAdapter extends XmlAdapter<UserImpl, User> {
+    public UserImpl marshal(User v) throws Exception {
+        if (v instanceof UserImpl) {
+            return (UserImpl)v;
+        }
+        return new UserImpl(v.getName());
+    }
+
+    public User unmarshal(UserImpl v) throws Exception {
+        return v;
+    }
+}
+
